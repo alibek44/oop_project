807800eb982ce0d599b7e1ec7fba9e49016b9789
alibek <alibekbekkaliev6@gmail.com>
2024-12-15 23:46:34 +0500

    somethingdfffgfg

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = uhihuiu" + i);
             System.out.println("HZ");gfhjk
-            //some
+            //somegf
         }
     }
 }
